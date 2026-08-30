957acd5c3f5fd5b49eec6f8b9a469ac10ec1a11e
Eden Fernando <eden@eandshome.net>
2026-08-30 11:19:00 +1000

    Initial Commit

---
+++ LessonManagmentApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LessonManagmentApp
+//
+//  Created by Eden Fernando on 30/8/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LessonManagmentApp/LessonManagmentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LessonManagmentAppApp.swift
+//  LessonManagmentApp
+//
+//  Created by Eden Fernando on 30/8/2026.
+//
+
+import SwiftUI
+
+@main
+struct LessonManagmentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
