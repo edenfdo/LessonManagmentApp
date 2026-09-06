59dd5066b7dec983e2d26214341152cb07c7ef26
Eden Fernando <eden@eandshome.net>
2026-09-06 16:35:43 +1000

    connected homeview to homeviewmodel

---
+++ LessonManagmentApp/Views/CalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarView.swift
+//  LessonManagmentApp
+//
+//  Created by Eden Fernando on 6/9/2026.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarView()
+}
