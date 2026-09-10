f998c4234a9abf63bb6d4d69ddf859474c75f786
Eden Fernando <eden@eandshome.net>
2026-09-10 12:18:08 +1000

    creates pages for leaderboard, profile, quizzes

---
+++ LessonManagmentApp/Views/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  LessonManagmentApp
+//
+//  Created by Eden Fernando on 10/9/2026.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
---
+++ LessonManagmentApp/Views/QuizzesView.swift
@@ -0,0 +1,18 @@
+//
+//  QuizzesView.swift
+//  LessonManagmentApp
+//
+//  Created by Eden Fernando on 10/9/2026.
+//
+
+import SwiftUI
+
+struct QuizzesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizzesView()
+}
