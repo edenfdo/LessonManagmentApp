526d0c8f9f444da38aa4d7656c6fc8b0b202ea52
Eden Fernando <eden@eandshome.net>
2026-09-10 12:15:19 +1000

    created resourcesview and menu bar

---
+++ LessonManagmentApp/Views/ResourcesView.swift
@@ -0,0 +1,18 @@
+//
+//  ResourcesView.swift
+//  LessonManagmentApp
+//
+//  Created by Eden Fernando on 10/9/2026.
+//
+
+import SwiftUI
+
+struct ResourcesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResourcesView()
+}
