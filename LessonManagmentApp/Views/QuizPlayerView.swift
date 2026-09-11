7d560e107d9805e8d32164f2d580fc6a005500a1
Eden Fernando <eden@eandshome.net>
2026-09-11 20:41:04 +1000

    worked on the quizzes page creating quizzes for treble clef and bass clef notes

---
+++ LessonManagmentApp/Views/QuizPlayerView.swift
@@ -0,0 +1,18 @@
+//
+//  QuizPlayerView.swift
+//  LessonManagmentApp
+//
+//  Created by Eden Fernando on 11/9/2026.
+//
+
+import SwiftUI
+
+struct QuizPlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizPlayerView()
+}
