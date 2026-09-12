15a39db3570ad4813e03b44693aba32d19798a6d
Eden Fernando <eden@eandshome.net>
2026-09-12 22:39:35 +1000

    refactor shared header into menubarview

---
+++ LessonManagmentApp/Views/MenuBarView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuBarView.swift
+//  LessonManagmentApp
+//
+//  Created by Eden Fernando on 12/9/2026.
+//
+
+import SwiftUI
+
+struct MenuBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuBarView()
+}
