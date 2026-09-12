cbbb335f9ea21ecd06137306a82c003b8629fe5f
Eden Fernando <eden@eandshome.net>
2026-09-12 23:06:19 +1000

    fix up settings and logout across student and teacher so they are standard

---
+++ LessonManagmentApp/Views/ChangePasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  ChangePasswordView.swift
+//  LessonManagmentApp
+//
+//  Created by Eden Fernando on 12/9/2026.
+//
+
+import SwiftUI
+
+struct ChangePasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChangePasswordView()
+}
