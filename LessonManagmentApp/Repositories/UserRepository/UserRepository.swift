2c924210bfe5197a78ea4eca98eb2ec11e75df6b
Eden Fernando <eden@eandshome.net>
2026-09-12 18:34:13 +1000

    swiftdata working for students

---
+++ LessonManagmentApp/Repositories/UserRepository/UserRepository.swift
@@ -0,0 +1,19 @@
+//
+//  UserRepository.swift
+//  LessonManagmentApp
+//
+//  Created by Eden Fernando on 12/9/2026.
+//
+
+import Foundation
+
+protocol UserRepository {
+
+    func getAllUsers() -> [User]
+
+    func getStudents() -> [User]
+
+    func getTeachers() -> [User]
+
+    func addUser(_ user: User)
+}
