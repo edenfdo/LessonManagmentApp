2f413f29b3a70b8a5712122d75fe5de87da3a1e2
Eden Fernando <eden@eandshome.net>
2026-09-12 19:33:53 +1000

    fixed up swiftdata for resources for both images and pdfs

---
+++ LessonManagmentApp/Views/TeacherResourcesView.swift
@@ -0,0 +1,18 @@
+//
+//  TeacherResourcesView.swift
+//  LessonManagmentApp
+//
+//  Created by Eden Fernando on 12/9/2026.
+//
+
+import SwiftUI
+
+struct TeacherResourcesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TeacherResourcesView()
+}
