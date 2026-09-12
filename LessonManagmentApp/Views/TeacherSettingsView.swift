e515332f7ea12bdd4e0709aacab30f996c5eac4a
Eden Fernando <eden@eandshome.net>
2026-09-12 17:57:02 +1000

    added settings page for teacher view

---
+++ LessonManagmentApp/Views/TeacherSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  TeacherSettingsView.swift
+//  LessonManagmentApp
+//
+//  Created by Eden Fernando on 12/9/2026.
+//
+
+import SwiftUI
+
+struct TeacherSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TeacherSettingsView()
+}
