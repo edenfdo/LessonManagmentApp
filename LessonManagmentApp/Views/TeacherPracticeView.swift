a3f7327993c59b44942f801890f3ba18b14237d4
Eden Fernando <eden@eandshome.net>
2026-09-12 17:27:43 +1000

    added practiceview for teacher and assignpracticetaskview

---
+++ LessonManagmentApp/Views/TeacherPracticeView.swift
@@ -0,0 +1,18 @@
+//
+//  TeacherPracticeView.swift
+//  LessonManagmentApp
+//
+//  Created by Eden Fernando on 12/9/2026.
+//
+
+import SwiftUI
+
+struct TeacherPracticeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TeacherPracticeView()
+}
