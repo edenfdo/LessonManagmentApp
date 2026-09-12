a5859d12a6ff359957971f2ad65bc994c19c63a5
Eden Fernando <eden@eandshome.net>
2026-09-12 17:24:27 +1000

    completed students page from teachers view

---
+++ LessonManagmentApp/Views/TeacherStudentDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  TeacherStudentDetailView.swift
+//  LessonManagmentApp
+//
+//  Created by Eden Fernando on 12/9/2026.
+//
+
+import SwiftUI
+
+struct TeacherStudentDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TeacherStudentDetailView()
+}
